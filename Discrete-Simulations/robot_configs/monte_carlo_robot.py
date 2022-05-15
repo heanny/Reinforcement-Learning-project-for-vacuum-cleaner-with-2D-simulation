c095f093465a5fe204d605dd76fb50cb325e4953
Jiong <ljiong1973@gmail.com>
2022-05-15 15:50:53 +0200

    Test version

--- Discrete-Simulations/robot_configs/monte_carlo_robot.py
+++ Discrete-Simulations/robot_configs/monte_carlo_robot.py
@@ -1,118 +1,156 @@
-from tkinter.tix import DirSelectBox
-import numpy as np
-import copy
+import random
 from collections import defaultdict
+from copy import deepcopy, copy
 
+import numpy as np
+import numpy.typing as npt
+from typing import Dict, Tuple, List, Optional
+from environment import Robot
 
-# initialization function
-def init_Q(n_rows, n_cols,dirs):
-    return np.full((4,n_rows, n_cols),float('-inf')) # TODO: initialize with 0 is bad because the value might be smaller than 0? Then the update ones 
-
-def make_epsilon_greedy_policy(n_rows,n_cols, Q, rewards,policy, num_actions=4,epsilon=0.1):
-    def policy_for_state(i,j):
-        A = np.ones(num_actions, dtype=float) * epsilon / num_actions
-        best_action = np.argmax(Q[:,i,j])
-        A[best_action] += (1.0 - epsilon)
-        return A
-    
-    policy = np.full((n_rows, n_cols), 0)     
-    for i in range(0,n_rows):
-        for j in range(0,n_cols):
-            if rewards[i][j] not in [-3,-1]:
-                policy[i][j]=policy_for_state(i,j)
-    return policy
-    
-
-def get_current_rewards(cells):
+@dataclass
+class Transition:
     """
-    Get the reward matrix based on grid's current circumstances(each tile's label) and robot's history.
-    :param cells: cells attribute of robot.grid, a matrix record the label of each tile
-    :param transformation: a punishment matrix, where each element is the punishment of each tile
-    :returns combined_reward: a reward matrix
+    Represents the mandatory fields of a transition.
+    It contains the probability of a transition, the next state due to the transition
+    and the reward.
     """
-    reward = copy.deepcopy(cells)
-    # label < -2L: this tile has a robot with different direction inside it. We set it to 0, meaning it is already clean.
-    reward[reward < -2] = 0
-    # label -2: this tile is an obstacle, we think they have the same function of wall tiles, so we reset as -1
-    reward[reward == -2] = -1
-    # label 3: death tile, give -3 to avoid robot reach it.
-    reward[reward == 3] = -3
-    max_value = np.max(reward)
-    if max_value < 1:
-        # After all the tiles have been cleared
-        # the robot must visit the death tile to terminate, so give it a high value 3
-        reward[reward == -3] = 3
-    return reward
+    probability: float
+    next_state: int
+    reward: int
 
-def episode_generation(i,j,dirs,rewards,policy):
-    # Generate an episode.
-    # An episode is an array of (state, action, reward) tuples
-    episode = []
-    dir_keys = dirs.get_keys()
-    state=(i,j)
-    done=False
-    for t in range(1e9):
-        probs = policy(state[0],state[1])
-        action = np.random.choice(np.arange(len(probs)), p=probs)
-        action_name =dir_keys[action]
-        # get the corresponding coordinates for an action e.g. 0 -> "n" -> row -1, column remains the same
-        direction=reversed(dirs[action_name]) # reverse to get the correct value for i and j
-        next_state = state+direction
-        # TODO: What if it hits obstacle or death tile
-        # if policy(next_state[0]next_state,[1]) is not :
-        #     done=True
-        reward=rewards(i,j)
-        state=(i,j)
-        episode.append((state, action_name, reward))
-        if done: # if the evaluation end
-            break # TODO
-        state = next_state
-    return episode
+class ModelFreeRL:
+    """
+    This class implements two mode-based reinforcement learning techniques: policy based
+    and value based.
+    """
 
-def Q_table(episode,Q,gamma=1.0):
-    returns_sum = defaultdict(float)
-    returns_count = defaultdict(float)
-    sa_in_episode = set([(x[0],x[1]) for x in episode])
-    for state, action in sa_in_episode:
-        sa_pair = (state, action)
-        # Calculate Q(s,a) for each (s,a) pair (mc policy evaluation)
-        # Find the first occurance of the (state, action) pair in the episode
-        first_occurence_idx = next(i for i,x in enumerate(episode)
-                                    if x[0] == state and x[1] == action)
-        # Sum up all rewards since the first occurance
-        G = sum([x[2]*(gamma**i) for i,x in enumerate(episode[first_occurence_idx:])])
-        # Calculate average return for this state over all sampled episodes
-        returns_sum[sa_pair] += G
-        returns_count[sa_pair] += 1.0
-        Q[state][action] = returns_sum[sa_pair] / returns_count[sa_pair]
-    return Q
+    def __init__(self, robot: Robot,gamma=0.1,epsilon = 0.1) -> None:
+        self.robot = robot
+        self.n_cols = robot.grid.n_rows
+        self.n_rows = robot.grid.n_cols
+        self.policy = np.full((4,self.n_rows, self.n_cols),0.25)
+        # self.Qvalue_table = self.init_Qvalue_table(self.n_rows, self.n_cols)
+        self.directions = ['n', 'e', 's', 'w']
+        self.direction_index_map = {'n': 0, 'e': 1, 's': 2, 'w': 3}
+        self.Q = np.zeros(4,self.n_rows,self.n_cols)
+
+    # def init_state_value(self):
+    #     state_space_cln = []
+    #     state_space_obs = []
+    #     for row in range(self._cells.shape[0]):
+    #         for col in range(self._cells.shape[1]):
+    #             if (-2 <= self._cells[row, col] < 0):
+    #                 state_space_obs.append(row*col)
+    #             else:
+    #                 state_space_cln.append(row*col)
+    #     return state_space_cln,state_space_obs
+
+    def simulation(self, robot, action, transformation):
+        # get reward of action
+        coordinate = robot.dirs[action]
+        possible_tiles = robot.possible_tiles_after_move()
+        reward = possible_tiles[coordinate]
+        if reward == 3:
+            reward = -2
+        if reward == -2:
+            reward = -1
+        # take action
+        while not action == robot.orientation:
+            # If we don't have the wanted orientation, rotate clockwise until we do:
+            robot.rotate('r')
+        print("start move")
+        robot.move()
+        print("end move")
+        # return the new state s' and reward
+        if reward == 0:
+            reward = transformation[robot.pos[0]][robot.pos[1]]
+        return robot.pos, reward
+
+    def Q_table(self, episode, Q, gamma=1.0):
+        returns_sum = defaultdict(float)
+        returns_count = defaultdict(float)
+        sa_in_episode = set([(x[0], x[1]) for x in episode])
+        for state, action in sa_in_episode:
+            sa_pair = (state, action)
+            # Calculate Q(s,a) for each (s,a) pair (mc policy evaluation)
+            # Find the first occurance of the (state, action) pair in the episode
+            first_occurence_idx = next(i for i, x in enumerate(episode)
+                                       if x[0] == state and x[1] == action)
+            # Sum up all rewards since the first occurance
+            G = sum([x[2] * (gamma ** i) for i, x in enumerate(episode[first_occurence_idx:])])
+            # Calculate average return for this state over all sampled episodes
+            returns_sum[sa_pair] += G
+            returns_count[sa_pair] += 1.0
+            Q[state[0],state[1],action] = returns_sum[sa_pair] / returns_count[sa_pair]
+        return Q
 
 
-def on_policy_mc_control(robot,rewards,max_iterations=10):
-    n_cols = robot.grid.n_rows 
-    n_rows = robot.grid.n_cols
-    dirs= robot.dirs
-    # initialization
-    Q=init_Q(n_rows, n_cols,dirs)
-    # first set the policy to be zero
-    policy=make_epsilon_greedy_policy(n_rows,n_cols,Q,rewards,policy,num_actions=4,epsilon=0.1) 
+    def generate_episode(self, policy,robot,transformation):
+        episode = []
 
-    # repeat till value converge:
-    for l in range (int(max_iterations)):
-        # generate an episode
-        episode = episode_generation(i=robot.pos[0],j=robot.pos[1],policy=policy)
-        Q = Q_table(episode,Q)
-        
-    return 0
+        robot_copy = copy.deepcopy(robot)
+        frequency = np.zeros((self.robot_copy.grid.n_rows, self.robot_copy.grid.n_cols))
+        while robot_copy.alive and np.max(robot_copy.grid.cells) > 0 and np.max(frequency) < 20:
+            print("+++++++++++++++++++++++ start +++++++++++++++++++++++++++++++")
+            print(robot_copy.alive, np.max(robot_copy.grid.cells))
+            # current state
+            state = robot_copy.pos
+            print("current state", state)
+            i = state[0]
+            j = state[1]
+            frequency[i,j] += 1
+            # use policy to choose action given state
+            policy_of_current_state = policy[:, i, j]
+            # for a in self.directions:
+            #     next_state: int = self._cells[(i + a[1]), (j + a[0])]
+            #     if (-2 <= next_state < 0):
+            action = random.choice(self.directions, p=policy_of_current_state)
 
-def robot_epoch(robot):
-    # initialize global variable "history" when starting the robot, to add the cleaned tiles in a matrix
-    # if not any(robot.history):
-    #     n_cols = robot.grid.n_rows
-    #     n_rows = robot.grid.n_cols
-    #     global history
-    #     history = np.full((n_rows, n_cols),0.0)
-    # history=np.where(history<99,history,99)
-    # transformation = np.where(history==0, history, -0.01*history) # the range of each element is (-1,0]
-    rewards = get_current_rewards(robot.grid.cells)
-    optimal_policy = on_policy_mc_control(robot,rewards)
+            # simulate and get s' and r
+            print("start simulation")
+            next_state, reward = self.simulation(robot_copy, action, transformation)
+            print("end simulation")
+            print(next_state, reward)
+            episode.append((state,action,reward))
+
+        return episode
+
+    def make_epsilon_greedy_policy(n_rows, n_cols, Q, rewards, policy, num_actions=4, epsilon=0.1):
+        def policy_for_state(i, j):
+            A = np.ones(num_actions, dtype=float) * epsilon / num_actions
+            best_action = np.argmax(Q[:, i, j])
+            A[best_action] += (1.0 - epsilon)
+            return A
+
+        for i in range(0, n_rows):
+            for j in range(0, n_cols):
+                if rewards[i][j] not in [-3, -1]:
+                    policy[i][j] = policy_for_state(i, j)
+        return policy
+
+    def on_policy_mc_control(self,robot, rewards,epsilon,transformation,gamma,max_iterations=10):
+        n_cols = robot.grid.n_rows
+        n_rows = robot.grid.n_cols
+        dirs = robot.dirs
+        # initialization
+        Q = self.Q_table()
+        policy = self.policy
+        # first set the policy to be zero
+        policy = self.make_epsilon_greedy_policy(n_rows, n_cols, Q, rewards, policy, num_actions=4, epsilon=0.1)
+
+        # repeat till value converge:
+        for l in range(int(max_iterations)):
+            # generate an episode
+            episode = self.generate_episode(policy,robot,transformation)
+            # Update Q table for each (s,a) in episode
+            Q = self.Q_table(episode,Q,gamma)
+            # TODO: Change |A(s)|
+            for item in episode: # (state,action,reward)
+                state=[item[0],item[1]]
+                best_action = np.argmax(Q[:,state[0], state[1]])
+                for action in self.robot.possible_tiles_after_move():
+                    if action == best_action:
+                        policy[state[0],state[1]] = 1-epsilon+epsilon/4
+                    else:
+                        policy[state[0],state[1]] = epsilon/4
+        return policy
