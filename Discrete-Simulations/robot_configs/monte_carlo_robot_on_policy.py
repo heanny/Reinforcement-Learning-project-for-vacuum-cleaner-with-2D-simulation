7e4bb30427daf5d40933c12ac2deee0e17f7579e
Sihan Zhu <30918855+SihanZhu22@users.noreply.github.com>
2022-05-19 21:29:47 +0200

    add comments

--- Discrete-Simulations/robot_configs/monte_carlo_robot_on_policy.py
+++ Discrete-Simulations/robot_configs/monte_carlo_robot_on_policy.py
@@ -4,66 +4,77 @@ from copy import deepcopy
 import numpy as np
 from mc import MC
 
+def on_policy_mc_control(MC): 
+    """
+    Implement the on policy Monte Carlo control
 
-def on_policy_mc_control(self):  # ,transformation
-    robot = self.robot
+    :param MC: the Monte Carlo class (from MC.py)
+    :returns policy: the optimal policy
+    """
+    robot = MC.robot
     n_cols = robot.grid.n_rows
     n_rows = robot.grid.n_cols
     dirs = robot.dirs
     # initialization
-    Q_tmp = deepcopy(self.Q)
-    policy = self.policy
-    # first set the policy to be zero
-    epsilon = self.epsilon
-    # repeat till value converge:
-    for l in range(int(self.max_iteration)):
+    Q_tmp = deepcopy(MC.Q)
+    policy = MC.policy
+    epsilon = MC.epsilon
+    # repeat for l iterations:
+    for l in range(int(MC.max_iteration)):
         # generate an episode
-        episode = self.generate_episode(policy)  # ,transformation
+        episode = MC.generate_episode(policy)
         # Update Q table for each (s,a) in episode
-        Q_tmp = self.Q_table(episode)
-
-        for item in episode:  # (state,action,reward)
+        Q_tmp = MC.Q_table(episode)
+        for item in episode:  # item: (state,action,reward)
             state = item[0]
+            # List of Q values of actions correspond to this state
             A = Q_tmp[:, state[0], state[1]]
+            # Find the action with the highest Q values
             best_action = np.argmax(A)
+            # In case there are more than one best actions: find all the best actions
             best_action_indices = np.where(A == Q_tmp[best_action, state[0], state[1]])
+            # update policy with epsilon-greedy
             for action in range(4):
                 if action in best_action_indices[0]:
                     policy[action, state[0], state[1]] = (1 - epsilon) / len(best_action_indices[0]) + epsilon / 4
                 else:
                     policy[action, state[0], state[1]] = epsilon / 4
-
     return policy
 
-
+# The robot epoch function for single processing
 def robot_epoch(robot):
-    model_free = MC(robot,gamma=0.3,epsilon=0.2,max_iteration=10)
+    # load MC model from class
+    model_free = MC(robot,gamma=0.6,epsilon=0.6,max_iteration=100)
+    # the optimal policy after Monte Carlo
     optimal_policy = on_policy_mc_control(model_free)
-    # print(optimal_policy)
     policy_of_current_state = optimal_policy[:, robot.pos[0], robot.pos[1]]
+    # indices of direction with the highest policy value
     indices = np.where(policy_of_current_state == np.max(policy_of_current_state))[0]
     probability = []
+    # epsilon-greedy
     for index in range(0, 4):
         if index in indices:
             probability.append(1/len(indices))
         else:
             probability.append(0)
     direction = np.random.choice(model_free.directions, p=probability)
-    # print("+++++++++++++++++++++++ move +++++++++++++++++++++++++++++++")
-    # print(direction)
     while not direction == robot.orientation:
         # If we don't have the wanted orientation, rotate clockwise until we do:
         robot.rotate('r')
     # Move:
     robot.move()
 
+# The robot epoch function for multiple processing
 def robot_epoch_(robot,gamma,epsilon):
-    model_free = MC(robot,gamma,epsilon,max_iteration=200)
+    # load MC model from class
+    model_free = MC(robot,gamma,epsilon,max_iteration=100)
+    # the optimal policy after Monte Carlo
     optimal_policy = on_policy_mc_control(model_free)
-    # print(optimal_policy)
     policy_of_current_state = optimal_policy[:, robot.pos[0], robot.pos[1]]
+    # indices of direction with the highest policy value
     indices = np.where(policy_of_current_state == np.max(policy_of_current_state))[0]
     probability = []
+    # epsilon-greedy
     for index in range(0, 4):
         if index in indices:
             probability.append(1/len(indices))
