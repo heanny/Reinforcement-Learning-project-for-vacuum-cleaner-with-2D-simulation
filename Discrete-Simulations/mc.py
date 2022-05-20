e4a459dd9a2dc9e124fbed30dbb626da8da68e42
Sihan Zhu <30918855+SihanZhu22@users.noreply.github.com>
2022-05-20 16:04:36 +0200

    update mc

--- Discrete-Simulations/mc.py
+++ Discrete-Simulations/mc.py
@@ -18,8 +18,8 @@ class MC:
         self.D = 0  # denominator of Q(s,a)
         self.policy = np.full((4,self.n_rows, self.n_cols),0.25) # initializing the policy matrix, which corresponds to epsilon-soft policy
         self.directions = ['n', 'e', 's', 'w']
-        self.direction_index_map = robot.dirs # list index corresponds to direction name
-        self.trans_dirs = {(0, -1):0, (1, 0): 1, (0, 1): 2, (-1, 0):3} # list index corrrespond to direction coordinate
+        self.direction_index_map = {'n': 0, 'e': 1, 's': 2, 'w': 3} # list index corresponds to direction name
+        self.trans_dirs = {(-1, 0):0, (0, 1): 1, (1, 0): 2, (0, -1):3} # list index corrrespond to direction coordinate
         self.Q = np.zeros((4,self.n_rows,self.n_cols)) # initializing Q table
         self.returns_sum = defaultdict(float)
         self.returns_count = defaultdict(float)
