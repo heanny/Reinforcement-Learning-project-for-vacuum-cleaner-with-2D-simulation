fb255fab25e3dd665bd0ab4b05017951fefd7411
Jin <41386026+heanny@users.noreply.github.com>
2022-05-21 06:24:25 +0200

    Update comments, final version

--- Discrete-Simulations/robot_configs/monte_carlo_robot_on_policy.py
+++ Discrete-Simulations/robot_configs/monte_carlo_robot_on_policy.py
@@ -63,7 +63,30 @@ def robot_epoch(robot):
 # The robot epoch function for multiple processing
 def robot_epoch_(robot,gamma,epsilon):
     # load MC model from class
-    model_free = MC(robot,gamma,epsilon,max_iteration=100)# '100': here to set the smaller iterations to get the heatmap.
+    model_free = MC(robot,gamma,epsilon,max_iteration=100)# '100': here to set the 200 iterations to get the heatmap in our report.
+    # the optimal policy after Monte Carlo
+    optimal_policy = on_policy_mc_control(model_free)
+    policy_of_current_state = optimal_policy[:, robot.pos[0], robot.pos[1]]
+    # indices of direction with the highest policy value
+    indices = np.where(policy_of_current_state == np.max(policy_of_current_state))[0]
+    probability = []
+    # epsilon-greedy
+    for index in range(0, 4):
+        if index in indices:
+            probability.append(1/len(indices))
+        else:
+            probability.append(0)
+    direction = np.random.choice(model_free.directions, p=probability)
+    while not direction == robot.orientation:
+        # If we don't have the wanted orientation, rotate clockwise until we do:
+        robot.rotate('r')
+    # Move:
+    robot.move()
+
+# The robot_epoch_a function for headless average
+def robot_epoch_a(robot):
+    # load MC model from class
+    model_free = MC(robot,gamma=0.6,epsilon=0.2,max_iteration=50)# '50': here to set 200 to get the table results in our report.
     # the optimal policy after Monte Carlo
     optimal_policy = on_policy_mc_control(model_free)
     policy_of_current_state = optimal_policy[:, robot.pos[0], robot.pos[1]]
